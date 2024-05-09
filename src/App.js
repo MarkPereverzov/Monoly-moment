c5703ae075a5bbb916340a6daeb2246995d7f034
Mark <mark.pereverzov@gmail.com>
2024-05-09 10:25:46 +0300

    Users and UserBoard

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import { useState } from 'react';
 
-let price = 100;
-let colors = ["brown", "red", "orange", "yellow", "green", "dark-green", "blue", "dark-blue", "purple", "dark-purple"];
-let images = ["unity", "unreal", "github", "audi", "amazon", "opencart", "shopee", "nike", "new-york", "adidas", "suzuki", "apple", "microsoft", "rockstar", "uplay", "play-station", "honda", "hp", "dell", "xiaomi", "spotify", "soundcloud", "deezer", "renault", "discord", "snapchat", "whatsapp"];
+let color = ["brown", "red", "orange", "yellow", "green", "dark-green", "blue", "dark-blue", "purple", "dark-purple"];
 
-function Block() {
+function Block(props) {
   return (
     <div className="space property">
 			<div className="container">
-				<img src={`img/${images[0]}.png`}/>
-				<div className={`color-bar ${colors[0]}`}>
-					<div className="price">{price}$</div>
+				<img src={`img/${props.image}.png`}/>
+				<div className={`color-bar ${color[props.color]}`}>
+					<div className="price">{props.price}$</div>
 				</div>
 			</div>
 		</div>
   );
 }
 
-function BlockAction() {
+function BlockAction(props) {
   return (
     <div className="space chance">
 			<div className="container">
-      <img src={`img/${images[1]}.png`}/>
+      <img src={`img/${props.image}.png`}/>
 			</div>
 		</div>
   );
 }
 
-function BlockCorner() {
+function BlockCorner(props) {
   return (
     <div className="space corner">
 			<div className="container">
-      <img src={`img/${images[2]}.png`}/>
+      <img src={`img/${props.image}.png`}/>
 			</div>
 		</div>
   );
@@ -43,57 +41,78 @@ function BlockCenter() {
   );
 }
 
+function BlockUser(props) {
+  return (
+    <div className="user">
+      <div className={`user-bar ${color[props.color]}`}></div>
+        <img src={`img/user.png`}/>
+        <div>
+          <a>Nickname</a>
+          <div className="balance">{props.balance}$</div>
+        </div>
+    </div>
+  );
+}
+
 export default function Board() {
   return (
     <>
-    <BlockCenter/>
-    <BlockCorner/>
-    <div className="row horizontal-row bottom-row">
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
+    <div className="userBoard">
+      <BlockUser balance="1000" color="1"/>
+      <BlockUser balance="1000" color="5"/>
+      <BlockUser balance="2000" color="7"/>
+      <BlockUser balance="2000" color="8"/>
     </div>
-    <BlockCorner/>
-    <div className="row vertical-row left-row">
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
-    </div>
-    <BlockCorner/>
-    <div className="row horizontal-row top-row">
-      <Block/>
-      <BlockAction/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
-    </div>
-    <BlockCorner/>
-    <div className="row vertical-row right-row">
-      <Block/>
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
-      <Block/>
-      <Block/>
-      <BlockAction/>
-      <Block/>
+    <div class="board">
+      <BlockCenter/>
+      <BlockCorner image="start"/>
+      <div className="row horizontal-row bottom-row">
+        <Block image="hp" color="1" price="100"/>
+        <Block image="dell" color="1" price="200"/>
+        <BlockAction image="question"/>
+        <Block image="xiaomi" color="1" price="300"/>
+        <Block image="honda" color="0" price="1000"/>
+        <Block image="unity" color="3" price="500"/>
+        <Block image="unreal" color="3" price="600"/>
+        <BlockAction image="question"/>
+        <Block image="github" color="3" price="700"/>
+      </div>
+      <BlockCorner image="go-to-jail"/>
+      <div className="row vertical-row left-row">
+        <Block image="amazon" color="8" price="100"/>
+        <Block image="opencart" color="8" price="100"/>
+        <BlockAction image="question-left"/>
+        <Block image="shopee" color="8" price="100"/>
+        <Block image="renault" color="0" price="1000"/>
+        <Block image="nike" color="6" price="100"/>
+        <Block image="new-york" color="6" price="100"/>
+        <BlockAction image="question-left"/>
+        <Block image="adidas" color="6" price="100"/>
+      </div>
+      <BlockCorner image="jail"/>
+      <div className="row horizontal-row top-row">
+        <Block image="rockstar" color="7" price="100"/>
+        <BlockAction image="question"/>
+        <Block image="uplay" color="7" price="100"/>
+        <Block image="apple" color="5" price="100"/>
+        <Block image="suzuki" color="0" price="1000"/>
+        <Block image="snapchat" color="4" price="100"/>
+        <Block image="whatsapp" color="4" price="100"/>
+        <BlockAction image="question"/>
+        <Block image="discord" color="4" price="100"/>
+      </div>
+      <BlockCorner image="chance"/>
+      <div className="row vertical-row right-row">
+        <Block image="spotify" color="2" price="100"/>
+        <Block image="soundcloud" color="2" price="100"/>
+        <Block image="deezer" color="2" price="100"/>
+        <BlockAction image="question-right"/>
+        <Block image="audi"color="0" price="1000"/>
+        <Block image="microsoft" color="5" price="100"/>
+        <Block image="xbox" color="9" price="100"/>
+        <BlockAction image="question-right"/>
+        <Block image="play-station" color="9" price="100"/>
+      </div>
     </div>
     </>
   );
